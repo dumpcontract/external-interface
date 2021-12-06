6c08f5717e5317846d3737dd2ef8125b3e303154
dumpcontract <93113169+dumpcontract@users.noreply.github.com>
2021-12-06 14:29:37 +0700

    Update IWETH.sol

--- IWETH.sol
+++ IWETH.sol
@@ -1,10 +1,29 @@
 
+
 interface IWETH {
+    function name() external view returns (string memory);
+
+    function symbol() external view returns (string memory);
+
+    function decimals() external view returns (uint8);
+
+    function balanceOf(address) external view returns (uint);
+
+    function allowance(address, address) external view returns (uint);
+
+    receive() external payable;
+
     function deposit() external payable;
 
-    function transfer(address to, uint value) external returns (bool);
-    
-    function withdraw(uint) external;
-    function balanceOf(address account) external view returns (uint256);
+    function withdraw(uint wad) external;
 
+    function totalSupply() external view returns (uint);
+
+    function approve(address guy, uint wad) external returns (bool);
+
+    function transfer(address dst, uint wad) external returns (bool);
+
+    function transferFrom(address src, address dst, uint wad)
+    external
+    returns (bool);
 }
